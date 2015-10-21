eccde41154686f6c1085c54f33d87e4bdb4d8692
YW <yuahwen.cheng@gmail.com>
2015-10-21 15:16:01 +0800

    Add brone color

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -20,7 +20,8 @@ public class ColorWheel {
             "#637a91", // dark gray
             "#f092b0", // pink
             "#b7c0c7",  // light gray
-            "#ddcs8b"   //gold
+            "#ddcs8b",   //gold
+            "#8b4513"   //bronze
     };
 
     // Method (abilities: things the object can do)
