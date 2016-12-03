ee62ae6d1afa65dd8e29e76a6a9b567b609d56f4
contra19 <contra19@gmail.com>
2016-12-03 09:52:56 -0500

    Merge branch 'add_gold_color'

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -20,13 +20,13 @@ public class ColorWheel {
             "#637a91", // dark gray
             "#f092b0", // pink
             "#b7c0c7", // light gray
-            "#8b4513"  // bronze
+            "#8b4513", // bronze
             "#ddcaab"  // gold
     };
 
     // Method (abilities: things the object can do)
     public int getColor() {
-        String color = "";
+        String color;
 
         // Randomly select a fact
         Random randomGenerator = new Random(); // Construct a new Random number generator
