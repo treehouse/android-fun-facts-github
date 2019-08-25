aa7a3ddbc4f16c5aab02d719cfc7655bf627e6f5
Ben Gooch <bengooch7@gmail.com>
2019-08-25 01:24:16 -0500

    Added Gold color

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -1,36 +1,42 @@
 package com.teamtreehouse.funfacts;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 
 import java.util.Random;
 
 public class ColorWheel {
     // Member variables (properties about the object)
-    public String[] mColors = {
-            "#39add1", // light blue
-            "#3079ab", // dark blue
-            "#c25975", // mauve
-            "#e15258", // red
-            "#f9845b", // orange
-            "#838cc7", // lavender
-            "#7d669e", // purple
-            "#53bbb4", // aqua
-            "#51b46d", // green
-            "#e0ab18", // mustard
-            "#637a91", // dark gray
-            "#f092b0", // pink
-            "#b7c0c7"  // light gray
-    };
+    public String[] mColors;
+
+    {
+        mColors = new String[]{
+                "#39add1", // light blue
+                "#3079ab", // dark blue
+                "#c25975", // mauve
+                "#e15258", // red
+                "#f9845b", // orange
+                "#838cc7", // lavender
+                "#7d669e", // purple
+                "#53bbb4", // aqua
+                "#51b46d", // green
+                "#e0ab18", // mustard
+                "#637a91", // dark gray
+                "#f092b0", // pink
+                "#b7c0c7",  // light gray
+                "#ddca8b"   // gold
+        };
+    }
 
     // Method (abilities: things the object can do)
     public int getColor() {
-        String color = "";
+        //String color = "";
 
         // Randomly select a fact
         Random randomGenerator = new Random(); // Construct a new Random number generator
         int randomNumber = randomGenerator.nextInt(mColors.length);
 
-        color = mColors[randomNumber];
+        String color = mColors[randomNumber];
         int colorAsInt = Color.parseColor(color);
 
         return colorAsInt;
